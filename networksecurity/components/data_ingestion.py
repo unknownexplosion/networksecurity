46e69b1f28fee63d9e470a24ee248d65ff3e58d2
Anubhav Mukherjee <anubhavmukherjee@Anubhavs-MacBook-Air.local>
2025-08-07 23:51:47 +0530

    Data transformation

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -21,10 +21,11 @@ MONGO_DB_URL=os.getenv("MONGO_DB_URL")
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
-            self.data_ingestion_config=data_ingestion_config
+            self.data_ingestion_config = data_ingestion_config
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
+
     def export_collection_as_dataframe(self):
         """
         Read data from mongodb
@@ -32,7 +33,6 @@ class DataIngestion:
         try:
             database_name=self.data_ingestion_config.database_name
             collection_name=self.data_ingestion_config.collection_name
-            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
             collection=self.mongo_client[database_name][collection_name]
 
             df=pd.DataFrame(list(collection.find()))
@@ -42,8 +42,8 @@ class DataIngestion:
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException
-        
+            raise NetworkSecurityException(e,sys)
+
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
@@ -52,10 +52,10 @@ class DataIngestion:
             os.makedirs(dir_path,exist_ok=True)
             dataframe.to_csv(feature_store_file_path,index=False,header=True)
             return dataframe
-            
+
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
+
     def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
             train_set, test_set = train_test_split(
@@ -66,13 +66,13 @@ class DataIngestion:
             logging.info(
                 "Exited split_data_as_train_test method of Data_Ingestion class"
             )
-            
+
             dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
-            
+
             os.makedirs(dir_path, exist_ok=True)
-            
-            logging.info(f"Exporting train and test file path.")
-            
+
+            logging.info("Exporting train and test file path.")
+
             train_set.to_csv(
                 self.data_ingestion_config.training_file_path, index=False, header=True
             )
@@ -80,13 +80,13 @@ class DataIngestion:
             test_set.to_csv(
                 self.data_ingestion_config.testing_file_path, index=False, header=True
             )
-            logging.info(f"Exported train and test file path.")
+            logging.info("Exported train and test file path.")
 
-            
+
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
-        
+
+
     def initiate_data_ingestion(self):
         try:
             dataframe=self.export_collection_as_dataframe()
@@ -97,4 +97,5 @@ class DataIngestion:
             return dataingestionartifact
 
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
+        finally:
